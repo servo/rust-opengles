556f8c8ab860e9e4589241b5f32285b510923266
Patrick Walton <pcwalton@mimiga.net>
2012-05-31 17:39:17 -0700

    Initial commit

---
+++ GLES2/gl2platform.h
@@ -0,0 +1,18 @@
+#ifndef GL2PLATFORM_H
+#define GL2PLATFORM_H
+
+#include <sys/types.h>
+#include <stdint.h>
+
+#define GL_APICALL
+#define GL_APIENTRY
+
+typedef int8_t khronos_int8_t;
+typedef uint8_t khronos_uint8_t;
+typedef float khronos_float_t;
+typedef int32_t khronos_int32_t;
+typedef intptr_t khronos_intptr_t;
+typedef ssize_t khronos_ssize_t;
+
+#endif
+
