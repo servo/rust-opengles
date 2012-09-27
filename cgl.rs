8108ff0eb6703cbd7b4996d8340f4450e72550ab
Patrick Walton <pcwalton@mimiga.net>
2012-09-27 13:57:29 -0700

    De-export cgl

--- cgl.rs
+++ cgl.rs
@@ -3,44 +3,44 @@
 use gl2::{GLenum, GLint, GLsizei, GLuint};
 use libc::*;
 
-type CGLContextObj = *c_void;
-type CGLError = c_int;
-type CGLPixelFormatAttribute = c_int;
-type CGLPixelFormatObj = *c_void;
-type IOSurfaceRef = *c_void;
+pub type CGLContextObj = *c_void;
+pub type CGLError = c_int;
+pub type CGLPixelFormatAttribute = c_int;
+pub type CGLPixelFormatObj = *c_void;
+pub type IOSurfaceRef = *c_void;
 
-const kCGLNoError: CGLError = 0;
+pub const kCGLNoError: CGLError = 0;
 
-const kCGLPFADoubleBuffer: CGLPixelFormatAttribute = 5;
-const kCGLPFACompliant: CGLPixelFormatAttribute = 83;
+pub const kCGLPFADoubleBuffer: CGLPixelFormatAttribute = 5;
+pub const kCGLPFACompliant: CGLPixelFormatAttribute = 83;
 
 #[nolink]
 extern {
     // CGLCurrent.h
 
-    fn CGLSetCurrentContext(ctx: CGLContextObj) -> CGLError;
-    fn CGLGetCurrentContext() -> CGLContextObj;
+    pub fn CGLSetCurrentContext(ctx: CGLContextObj) -> CGLError;
+    pub fn CGLGetCurrentContext() -> CGLContextObj;
 
     // OpenGL.h
 
     // Pixel format functions
-    fn CGLChoosePixelFormat(attribs: *CGLPixelFormatAttribute, pix: *CGLPixelFormatObj,
-                            npix: *GLint) -> CGLError;
-    fn CGLDestroyPixelFormat(pix: CGLPixelFormatObj);
+    pub fn CGLChoosePixelFormat(attribs: *CGLPixelFormatAttribute, pix: *CGLPixelFormatObj,
+                                npix: *GLint) -> CGLError;
+    pub fn CGLDestroyPixelFormat(pix: CGLPixelFormatObj);
 
     // Context functions
-    fn CGLCreateContext(pix: CGLPixelFormatObj, share: CGLContextObj, ctx: *CGLContextObj) ->
-                        CGLError;
+    pub fn CGLCreateContext(pix: CGLPixelFormatObj, share: CGLContextObj, ctx: *CGLContextObj) ->
+                            CGLError;
 
     // Locking functions
-    fn CGLLockContext(ctx: CGLContextObj) -> CGLError;
-    fn CGLUnlockContext(ctx: CGLContextObj) -> CGLError;
+    pub fn CGLLockContext(ctx: CGLContextObj) -> CGLError;
+    pub fn CGLUnlockContext(ctx: CGLContextObj) -> CGLError;
 
     // CGLIOSurface.h
 
-    fn CGLTexImageIOSurface2D(ctx: CGLContextObj, target: GLenum, internal_format: GLenum,
-                              width: GLsizei, height: GLsizei, format: GLenum, ty: GLenum,
-                              ioSurface: IOSurfaceRef, plane: GLuint) -> CGLError;
+    pub fn CGLTexImageIOSurface2D(ctx: CGLContextObj, target: GLenum, internal_format: GLenum,
+                                  width: GLsizei, height: GLsizei, format: GLenum, ty: GLenum,
+                                  ioSurface: IOSurfaceRef, plane: GLuint) -> CGLError;
 }
 
 #[nolink]
