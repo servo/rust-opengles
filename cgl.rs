b5ad2c95446d0e7074f2670490c950d2429b90e5
Jack Moffitt <jack@metajack.im>
2013-11-19 10:31:21 -0700

    Add in missing CGL stuff and fix mutability of externs.

--- cgl.rs
+++ cgl.rs
@@ -22,8 +22,86 @@ pub type IOSurfaceRef = *c_void;
 
 pub static kCGLNoError: CGLError = 0;
 
+pub static kCGLPFAAllRenderers: CGLPixelFormatAttribute = 1;
 pub static kCGLPFADoubleBuffer: CGLPixelFormatAttribute = 5;
+pub static kCGLPFAStereo: CGLPixelFormatAttribute = 6;
+pub static kCGLPFAAuxBuffers: CGLPixelFormatAttribute = 7;
+pub static kCGLPFAColorSize: CGLPixelFormatAttribute = 8;
+pub static kCGLPFAAlphaSize: CGLPixelFormatAttribute = 11;
+pub static kCGLPFADepthSize: CGLPixelFormatAttribute = 12;
+pub static kCGLPFAStencilSize: CGLPixelFormatAttribute = 13;
+pub static kCGLPFAAccumSize: CGLPixelFormatAttribute = 14;
+pub static kCGLPFAMinimumPolicy: CGLPixelFormatAttribute = 51;
+pub static kCGLPFAMaximumPolicy: CGLPixelFormatAttribute = 52;
+pub static kCGLPFAOffScreen: CGLPixelFormatAttribute = 53;
+pub static kCGLPFAFullScreen: CGLPixelFormatAttribute = 54;
+pub static kCGLPFASampleBuffers: CGLPixelFormatAttribute = 55;
+pub static kCGLPFASamples: CGLPixelFormatAttribute = 56;
+pub static kCGLPFAAuxDepthStencil: CGLPixelFormatAttribute = 57;
+pub static kCGLPFAColorFloat: CGLPixelFormatAttribute = 58;
+pub static kCGLPFAMultisample: CGLPixelFormatAttribute = 59;
+pub static kCGLPFASupersample: CGLPixelFormatAttribute = 60;
+pub static kCGLPFASampleAlpha: CGLPixelFormatAttribute = 61;
+pub static kCGLPFARendererID: CGLPixelFormatAttribute = 70;
+pub static kCGLPFASingleRenderer: CGLPixelFormatAttribute = 71;
+pub static kCGLPFANoRecovery: CGLPixelFormatAttribute = 72;
+pub static kCGLPFAAccelerated: CGLPixelFormatAttribute = 73;
+pub static kCGLPFAClosestPolicy: CGLPixelFormatAttribute = 74;
+pub static kCGLPFARobust: CGLPixelFormatAttribute = 75;
+pub static kCGLPFABackingStore: CGLPixelFormatAttribute = 76;
+pub static kCGLPFAMPSafe: CGLPixelFormatAttribute = 78;
+pub static kCGLPFAWindow: CGLPixelFormatAttribute = 80;
+pub static kCGLPFAMultiScreen: CGLPixelFormatAttribute = 81;
 pub static kCGLPFACompliant: CGLPixelFormatAttribute = 83;
+pub static kCGLPFADisplayMask: CGLPixelFormatAttribute = 84;
+pub static kCGLPFAPBuffer: CGLPixelFormatAttribute = 90;
+pub static kCGLPFARemotePBuffer: CGLPixelFormatAttribute = 91;
+pub static kCGLPFAAllowOfflineRenderers: CGLPixelFormatAttribute = 96;
+pub static kCGLPFAAcceleratedCompute: CGLPixelFormatAttribute = 97;
+pub static kCGLPFAOpenGLProfile: CGLPixelFormatAttribute = 99;
+pub static kCGLPFAVirtualScreenCount: CGLPixelFormatAttribute = 128;
+
+pub static CORE_BOOLEAN_ATTRIBUTES: &'static [CGLPixelFormatAttribute] =
+    &[kCGLPFAAllRenderers,
+      kCGLPFADoubleBuffer,
+      kCGLPFAStereo,
+      kCGLPFAAuxBuffers,
+      kCGLPFAMinimumPolicy,
+      kCGLPFAMaximumPolicy,
+      kCGLPFAOffScreen,
+      kCGLPFAFullScreen,
+      kCGLPFAAuxDepthStencil,
+      kCGLPFAColorFloat,
+      kCGLPFAMultisample,
+      kCGLPFASupersample,
+      kCGLPFASampleAlpha,
+      kCGLPFASingleRenderer,
+      kCGLPFANoRecovery,
+      kCGLPFAAccelerated,
+      kCGLPFAClosestPolicy,
+      kCGLPFARobust,
+      kCGLPFABackingStore,
+      kCGLPFAMPSafe,
+      kCGLPFAWindow,
+      kCGLPFAMultiScreen,
+      kCGLPFACompliant,
+      kCGLPFAPBuffer,
+      kCGLPFARemotePBuffer,
+      kCGLPFAAllowOfflineRenderers,
+      kCGLPFAAcceleratedCompute];
+
+pub static CORE_INTEGER_ATTRIBUTES: &'static [CGLPixelFormatAttribute] =
+    &[kCGLPFAColorSize,
+      kCGLPFAAlphaSize,
+      kCGLPFADepthSize,
+      kCGLPFAStencilSize,
+      kCGLPFAAccumSize,
+      kCGLPFASampleBuffers,
+      kCGLPFASamples,
+      kCGLPFARendererID,
+      kCGLPFADisplayMask,
+      kCGLPFAOpenGLProfile,
+      kCGLPFAVirtualScreenCount];
 
 #[nolink]
 extern {
@@ -35,8 +113,13 @@ extern {
     // OpenGL.h
 
     // Pixel format functions
-    pub fn CGLChoosePixelFormat(attribs: *CGLPixelFormatAttribute, pix: *CGLPixelFormatObj,
-                                npix: *GLint) -> CGLError;
+    pub fn CGLChoosePixelFormat(attribs: *CGLPixelFormatAttribute,
+                                pix: *mut CGLPixelFormatObj,
+                                npix: *mut GLint) -> CGLError;
+    pub fn CGLDescribePixelFormat(pix: CGLPixelFormatObj,
+                                  pix_num: GLint,
+                                  attrib: CGLPixelFormatAttribute,
+                                  value: *mut GLint) -> CGLError;
     pub fn CGLDestroyPixelFormat(pix: CGLPixelFormatObj);
 
     // Context functions
