caabe09000e102fb2a9d8d88ebf7561742f982ec
Youngmin Yoo <youngmin.yoo@samsung.com>
2013-11-28 15:02:08 +0900

    Add crate lib

--- lib.rs
+++ lib.rs
@@ -7,6 +7,10 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
+#[link(name = "opengles",
+        vers = "0.1")];
+#[crate_type = "lib"];
+
 extern mod std;
 
 pub mod gl2;
